5ad26ddaf90c9a7898d28b6bb4919d55f88d8588
Slava Kulichenko <vku@dbbsoftware.com>
2025-03-12 11:13:40 +0200

    include milliseconds

--- src/utils/convertSeconds.js
+++ src/utils/convertSeconds.js
@@ -1,5 +1,6 @@
 export default function (seconds) {
     const minutes = String(Math.floor(seconds / 60)).padStart(2, '0')
     const remainingSeconds = String(Math.floor(seconds % 60)).padStart(2, '0')
-    return `${minutes}:${remainingSeconds}`
+    const milliseconds = String(Math.floor((seconds % 1) * 1000)).padStart(3, '0')
+    return `${minutes}:${remainingSeconds}:${milliseconds}`
 }
